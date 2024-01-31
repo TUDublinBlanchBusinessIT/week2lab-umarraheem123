90f5154d497908c4f792a69cea48f7f91728384a
B00152481 Umar Raheem <b00152481@mytudublin.ie>
2024-01-31 12:49:19 +0000

    Completed PartC

---
+++ PartC.py
@@ -0,0 +1,12 @@
+#Umar Raheem
+#31/01/2024
+#A Program to Print Steps 1 to 9 to the Screen
+
+def printSteps (a, b):
+    for i in range (a, b+1):
+        print(f"Steps (i)")
+ 
+
+
+printSteps (1, 9)         
+
