4ea8d653a433a55d6e6009cfff9b9f96ce0c8f30
B00152481 Umar Raheem <b00152481@mytudublin.ie>
2024-01-31 12:39:12 +0000

    PartA.py file added

--- PartA.py
+++ PartA.py
@@ -2,12 +2,15 @@
 #31/01/2024
 #A Program to Print Steps 1 to 9 to the Screen
 
+def printSteps ():
+    print("Step 4")
+    print("Step 5")
+    print("Step 6")
+
 print("Step 1")
 print("Step 2")
 print("Step 3")
-print("Step 4")
-print("Step 5")
-print("Step 6")
+printSteps ()
 print("Step 7")
 print("Step 8")
 print("Step 9")
